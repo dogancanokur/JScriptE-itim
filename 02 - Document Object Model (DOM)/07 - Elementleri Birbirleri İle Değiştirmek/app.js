82d34e4eb46516576b76bbe85c5c2e5418c0bf3a
dogancanokur <ahmet.okur.94@gmail.com>
2019-10-28 01:43:11 +0300

    element değiştirme

--- 02 - Document Object Model (DOM)/07 - Elementleri Birbirleri İle Değiştirmek/app.js
+++ 02 - Document Object Model (DOM)/07 - Elementleri Birbirleri İle Değiştirmek/app.js
@@ -1,1 +1,20 @@
-alert("sd")
+// Replace -- değiştirmek
+// <h5 class="card-title" id="tasks-title">Todolar</h5>
+const cardbody = document.querySelectorAll(".card-body")[1];
+
+// h5 ile h3 değiştiriyoruz.
+
+const newElement = document.createElement("h3");
+
+newElement.className = "card-title";
+newElement.id = "task-title";
+newElement.textContent = "Yeni Listeler";
+
+// Eski Element
+
+const oldElement = document.querySelector("#tasks-title");
+
+cardbody.replaceChild(newElement,oldElement);
+
+console.log(newElement);
+console.log(cardbody);
